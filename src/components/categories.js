315156dbeab839a8e1d352ad9478c85201894aa3
Niño Rey Noble <nobleninorey@gmail.com>
2024-03-26 15:54:21 +0800

    Used map feature in categories
    Used map feature in categories

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,16 +1,25 @@
 import CategoryItem from "./categoryItem";
 import "./index-style.css";
+
+const categories = [
+  { type: "Earrings", image: "img/earrings.jpg" },
+  { type: "Necklace", image: "img/necklace.jpg" },
+  { type: "Bracelet", image: "img/bracelet.jpg" },
+  { type: "Ring", image: "img/rings.jpg" },
+];
+
 export default function Categories() {
   return (
     <section className="categories-container">
-      <h2 className="categories-title">
-        Categories 
-      </h2>
+      <h2 className="categories-title">Categories</h2>
       <section className="categories">
-        <CategoryItem type="Earrings" image="img/earrings.jpg"/>
-        <CategoryItem type="Necklace" image ="img/necklace.jpg"/>
-        <CategoryItem type="Bracelet" image="img/bracelet.jpg"/>
-        <CategoryItem type="Ring" image="img/rings.jpg"/>
+        {categories.map((category) => (
+          <CategoryItem
+            key={category.type}
+            type={category.type}
+            image={category.image}
+          />
+        ))}
       </section>
     </section>
   );
